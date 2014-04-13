ea11ac0ae972b6dc833c05d6bb6a85c18bfe1650
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-04-13 11:39:52 -0400

    script to fix turn line of http to markdown link

--- correctLinks.R
+++ correctLinks.R
@@ -1,7 +1,14 @@
 #' Find unlinked hyperlinks in a markdown file and turn them into links
 #' 
-con<-file("index.md","rw")
-lines<-readLines(con)
-for(line in lines){
-  
+incon<-file("index.md","rw")
+outcon<-file("index3.md","w")
+lines<-readLines(incon)
+for(line in 1:length(lines)){
+  if(regexpr("^https?://",lines[line])[1]==1){
+    lines[line]<-paste("[",lines[line],"]",
+                       "(",lines[line],")",sep="")
+  }
 }
+writeLines(lines,outcon)
+close(incon)
+close(outcon)
